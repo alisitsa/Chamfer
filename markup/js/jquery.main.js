6f9a8e45d1250991e58a88e210d3b3356fcf2383
design interior <Inoi2@yandex.ru>
2016-09-13 11:45:43 +0300

    последние изменения

--- markup/js/jquery.main.js
+++ markup/js/jquery.main.js
@@ -15,3 +15,34 @@ nav.find("a").each(function() {
 $("nav select").change(function() {
 	window.location = $(this).find("option:selected").val();
 });
+
+var navi = $("#portfolio-nav");
+$("<select class='nav-select visible-xs' />").appendTo(navi);
+var select = navi.find("select");
+
+// Populate dropdown with menu items
+navi.find("a").each(function() {
+	var el = $(this);
+		$("<option />", {
+		"value"   : el.attr("href"),
+		"text"    : el.text()
+	}).appendTo("navi select");
+});
+
+$("navi select").change(function() {
+	window.location = $(this).find("option:selected").val();
+});
+
+$(function(){
+	$('#gallery').slick({
+		dots: true, 
+		autoplay: false,
+		slidesToShow: 2,
+  		slidesToScroll: 2,
+	});
+
+	$('.js--features-gallery').slick({
+		arrows: false,
+		dots: true
+	});
+})
